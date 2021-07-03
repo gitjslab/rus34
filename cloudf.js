e796e40c8c4b611fcd3a48d27f6d3451a6338705
gitjslab <86866390+gitjslab@users.noreply.github.com>
2021-07-03 14:29:22 +0530

    Update cloudf.js

--- cloudf.js
+++ cloudf.js
@@ -1,4 +1,14 @@
-$(document).load(function () {
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}   
+
+docReady(function() {
     var regobject = {};
     new FormData(document.getElementById('formprofileupdate')).forEach((value, key) => regobject[key] = value);
     var regjson = JSON.stringify(regobject);
